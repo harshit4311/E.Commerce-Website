fa38f53953b4e0a8f059c407387d63f8170ed194
Harshit Shah <139583216+harshit4311@users.noreply.github.com>
2023-12-02 13:48:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
